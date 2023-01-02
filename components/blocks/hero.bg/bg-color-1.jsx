90c8eb7c86f4024d87b34f1a49cdea93a09f9439
Andrew Nelson <andrew@anothercreative.ca>
2023-01-02 05:00:24 +0000

    // mute svg bg

--- components/blocks/hero.bg/bg-color-1.jsx
+++ components/blocks/hero.bg/bg-color-1.jsx
@@ -14,7 +14,7 @@ export default function bgColor1() {
 				<div className={`${hero.barGradient}`}></div>
 			</div>
 			<div className={`${hero.radialContainer} noEvents`}>
-				<Image src="/img/dataism-24.svg" className={`${hero.svg_background}`} height={2000} width={2000} alt="Dataism 24 by Colorpong: https://ywft.us/2c4d58b21" />
+				{/* <Image src="/img/dataism-24.svg" className={`${hero.svg_background}`} height={2000} width={2000} alt="Dataism 24 by Colorpong: https://ywft.us/2c4d58b21" /> */}
 				<div className={`${hero.radialGradient}`}></div>
 			</div>
 		</div>

037bfa5a57517d481d879be4faec399cc7854750
Andrew Nelson <andrew@anothercreative.ca>
2023-01-13 16:05:48 +0000

    add rel="noopener"

--- components/structure/layout/footer.jsx
+++ components/structure/layout/footer.jsx
@@ -33,25 +33,25 @@ export default function Footer() {
 					<ul className={css.thanks}>
 						<li><h4>Acknowledgments</h4></li>
 						<li>
-							<a href="" target="_blank">Jihad Hassan - Marketing Director <Icon icon={[ 'fad', 'arrow-up-right-from-square' ]} /></a>
+							<a href="" rel="noopener" target="_blank">Jihad Hassan - Marketing Director <Icon icon={[ 'fad', 'arrow-up-right-from-square' ]} /></a>
 							<p>Thank you for all the advice and feedback.</p>
 						</li>
 						<li>	
-							<a href="https://www.youworkforthem.com/designer/536/colorpong?aff=1115" target="_blank">Colorpong - Artist <Icon icon={[ 'fad', 'arrow-up-right-from-square' ]} /></a>
+							<a href="https://www.youworkforthem.com/designer/536/colorpong?aff=1115" rel="noopener" target="_blank">Colorpong - Artist <Icon icon={[ 'fad', 'arrow-up-right-from-square' ]} /></a>
 							<p>Checkout their amazing vector illustrations. </p>
 						</li>
 						<li>
-							<a href="https://brittanychiang.com/" target="_blank">Brittany Chiang - Software Engineer <Icon icon={[ 'fad', 'arrow-up-right-from-square' ]} /></a> 
+							<a href="https://brittanychiang.com/" rel="noopener" target="_blank">Brittany Chiang - Software Engineer <Icon icon={[ 'fad', 'arrow-up-right-from-square' ]} /></a> 
 							<p>A major inspiration behind this open source theme.</p>
 						</li>
-						<li><a href="https://vercel.com/docs" target="_blank">Vercel - Platform <Icon icon={[ 'fad', 'arrow-up-right-from-square' ]} /></a>
+						<li><a href="https://vercel.com/docs" rel="noopener" target="_blank">Vercel - Platform <Icon icon={[ 'fad', 'arrow-up-right-from-square' ]} /></a>
 							<p>Host your own Next.js project for free 🥳</p>
 						</li>
 					</ul>
 					<ul className={css.links}>
 						<li><h4>Links</h4></li>
 						<li>
-							<a href="https://www.youworkforthem.com/?aff=1115" target="_blank">YWFT Creative Marketplace <Icon icon={[ 'fad', 'arrow-up-right-from-square' ]} /></a>
+							<a href="https://www.youworkforthem.com/?aff=1115" rel="noopener" target="_blank">YWFT Creative Marketplace <Icon icon={[ 'fad', 'arrow-up-right-from-square' ]} /></a>
 							<p>Best in class for assets.</p>
 						</li>
 					</ul>
@@ -66,7 +66,7 @@ export default function Footer() {
 					</ul>
 				</section>
 				<section className={css.github}>
-					<a href="https://github.com/atlamors/portfolio-theme" target="_blank">
+					<a href="https://github.com/atlamors/portfolio-theme"rel="noopener"  target="_blank">
 						<h5>Steal this theme! Its open-source 🥰</h5>
 						<ul>
 							<li>

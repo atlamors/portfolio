ce2dbf8ff9b9c7b24c332af303422ce29d9d5821
Andrew Nelson <andrew@anothercreative.ca>
2023-01-23 17:37:17 +0000

    add animations

--- components/utils/badge.list.util.jsx
+++ components/utils/badge.list.util.jsx
@@ -1,51 +1,88 @@
+import { useEffect } from 'react'
+import { m, useAnimation } from "framer-motion"
+import { useInView } from 'react-intersection-observer'
+
 // Utility components
 import Icon from '../utils/icon.util.jsx'
 
 /**
- * scss reference for utils should probably be pulled in from the first component under the section
- */
+* scss reference for utils should probably be pulled in from the first component under the section
+*/
 import badges from '../../styles/blocks/badges.module.scss';
 
-/**
- * 
- * @param {array} 	list of badges to populate
- * @param {string}	block name for class name
- * @param {string} 	invertedColor string to assign inverted color class. //Refactor this prop
- * @returns <Badges />
- */
+
 export default function Badges({ list, block, invertedColor }) {
+
+	const controls = useAnimation();
+	const { ref, inView  } = useInView({
+		"threshold": 0.5,
+		"triggerOnce": true
+	})
+
+	useEffect( () => {
+		if ( inView ) {	controls.start("visible") }
+	}, [ controls, inView ] );
+
+	const container = {
+		hidden: { opacity: 1 },
+		visible: {
+			opacity: 1,
+			transition: {
+				delayChildren: 0.05,
+				staggerChildren: 0.05
+			}
+		}
+	}
+	
+	const item = {
+		hidden: { y: 40, opacity: 0 },
+		visible: {
+			y: 0,
+			opacity: 1
+		}
+	}
+
+
 	return (
-		<ul className={`${badges.list} ${badges[block]} ${badges[invertedColor]}`}>
-			{
-			list.map( ({ key, name, type }) => {
-				return ( 
-					<li key={name} className={`${badges.item} ${key}`}>
-						<IconModule iconKey={key} iconType={type} />
-						<span className={badges.title}>{name}</span>
-					</li> 
+		<m.ul
+			className={`${badges.list} ${badges[block]} ${badges[invertedColor]}`}
+			//Animations
+				ref={ref}
+				variants={container}
+				initial="hidden"
+				animate={controls}
+		>
+		{
+		list.map( ({ key, name, type }) => {
+			return ( 
+				<m.li 
+					key={name} 
+					className={`${badges.item} ${key}`}
+					//Animations
+						variants={item} 
+				>
+					<IconModule iconKey={key} iconType={type} />
+					<span className={badges.title}>{name}</span>
+				</m.li> 
 				)
 			}) 
-			}
-		</ul>
+		}
+		</m.ul>
 	)
 }
 
-/**
- * 
- * @param {*} param0 
- * @returns 
- */
 function IconModule({ iconKey, iconType }) {
 	switch (iconType) {
 		case 'far':
 		case 'fad':
 		case 'fat':
 		case 'fas':
-			return ( <Icon icon={[ iconType, iconKey ]} /> )
+		return ( <Icon icon={[ iconType, iconKey ]} /> )
 		case 'devicon':
-			return ( <i className={`devicon-${iconKey}-plain colored`} /> )
+		return ( <i className={`devicon-${iconKey}-plain colored`} /> )
 		default:
-			return ( '' )
+		return ( '' )
 	}
 }
-
+		
+		

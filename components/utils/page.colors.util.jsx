57cf6010c40d421d487a5e2f98ddc07934cfd7b4
Andrew Nelson <andrew@anothercreative.ca>
2023-01-23 17:37:26 +0000

    add svg support

--- components/utils/page.colors.util.jsx
+++ components/utils/page.colors.util.jsx
@@ -1,10 +1,18 @@
+import css from '../../styles/utils/page.colors.module.scss'
+
 export default function ColorOverrides({ colors }) {
 	return (
 		<>	
-			<data id="page-specific-colors">
+			<data id="page-specific-colors" className={css.colors}>
 				<Dark colors={colors.dark} />
 				<Unicorn colors={colors.unicorn} />
 				<Light colors={colors.light} />
+				<svg ariaHidden="true" focusable="false">
+					<linearGradient id="fa-gradient" x1="0%" y1="0%" x2="175%" y2="175%">
+						<stop offset="0%" stopColor="var(--neon-1-2)" />
+						<stop offset="100%" stopColor="var(--neon-1-1)" />
+					</linearGradient>
+				</svg>
 			</data>
 		</>
 	)

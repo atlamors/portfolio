cd61fb4989f05372a708438f6b18506e606e466b
Andrew Nelson <andrew@anothercreative.ca>
2023-05-03 01:32:25 +0000

    turn off lfw

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,7 +16,7 @@ export default function HomePage() {
 		<>
 			<Color colors={colors} />
 			<Hero />
-			<Looking />
+			{/* <Looking /> */}
 			<FeaturedProjects />
 			<About />
 			<Technical />
